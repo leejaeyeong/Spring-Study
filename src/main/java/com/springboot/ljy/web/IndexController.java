59441dd3c9a68bdbb7ec4b72f9b032c8fe6253a3
이재영 <dldustn14@gmail.com>
2020-06-04 01:43:52 +0900

    Change http session to annotation based session

--- src/main/java/com/springboot/ljy/web/IndexController.java
+++ src/main/java/com/springboot/ljy/web/IndexController.java
@@ -23,10 +23,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
